ee8abda9647f67c02b874337044645c11dc759cc
Chen-GuanJie <60101474+Chen-GuanJie@users.noreply.github.com>
2023-11-03 14:52:20 +0800

    add exception test

--- src/test/java/com/MainTest.java
+++ src/test/java/com/MainTest.java
@@ -18,4 +18,14 @@ class MainTest {
 		int result = main.add(4, 1);
 		assertEquals(5, result);
 	}
+	
+	@Rule
+	public ExpectedException exception = ExpectedException.none()
+	@test
+	void test2() throws IllegalArgumentException{
+		exception.expect(IllegalArgumentException.class)
+		exception.expectMessage("Both numbers must be non-negative.")
+		Main main = new Main();
+		int result = main.add(-4, -1);
+	}
 }
