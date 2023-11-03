f60bdf44b88bc9a6ad6dbda7514b79b787bb7ca7
Chen-GuanJie <60101474+Chen-GuanJie@users.noreply.github.com>
2023-11-03 14:43:09 +0800

    fix and add test

--- src/test/java/com/MainTest.java
+++ src/test/java/com/MainTest.java
@@ -12,5 +12,10 @@ class MainTest {
 		int result = main.add(1, 1);
 		assertEquals(2, result);
 	}
-
+	@Test
+	void test2() {
+		Main main = new Main();
+		int result = main.add(4, 1);
+		assertEquals(5, result);
+	}
 }
